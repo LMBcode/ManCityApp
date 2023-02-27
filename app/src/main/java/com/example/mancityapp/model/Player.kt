8b062e348bccc4a2781bae801e944f05c067c704
Mombacke <mouhamed@ezfirecontrols>
2023-02-27 21:37:49 +0100

    adding players / menu screen

---
+++ app/src/main/java/com/example/mancityapp/model/Player.kt
@@ -0,0 +1,10 @@
+package com.example.mancityapp.model
+
+data class Player(
+    val name : String,
+    val surname : String,
+    val position : String,
+    val shirtNumber : Int,
+    val age : Int,
+    val nationality : String
+)
